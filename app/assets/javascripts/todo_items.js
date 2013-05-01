08367996647a51faa1352c2132ecb4a078e26941
Erik Michaels-Ober <sferik@gmail.com>
2013-05-01 14:51:23 -0700

    Clear input box and return focus

--- app/assets/javascripts/todo_items.js
+++ app/assets/javascripts/todo_items.js
@@ -2,7 +2,8 @@ $(document).ready(function(){
   $('form').on('submit', function(event){
     event.preventDefault();
     var form = $(this);
-    var inputValue = $('#todo_item_name').val();
+    var input = $('#todo_item_name');
+    var inputValue = input.val();
     var year = $('#todo_item_due_at_1i').val();
     var month = $('#todo_item_due_at_2i').val();
     var day = $('#todo_item_due_at_3i').val();
@@ -22,6 +23,8 @@ $(document).ready(function(){
         var deleteButton = $('<span class="item_delete"><a href="/todo_items/' + todo.id + '" data-confirm="Are you sure?" data-method="delete" rel="nofollow"><span><object data="/assets/trash.svg" type="image/svg+xml"><img alt="Trash" src="/assets/trash.png" /></object></span></a></span>');
         entry.append(checkbox, name, time, deleteButton);
         entry.appendTo(list);
+        input.val('');
+        input.focus();
       }
     });
   });
