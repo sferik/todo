aeb83dd6266b7df857e5d6359ee0cd680fea03a9
Erik Michaels-Ober <sferik@gmail.com>
2013-05-01 10:55:04 -0700

    Resize and align trash icon

--- app/helpers/todo_items_helper.rb
+++ app/helpers/todo_items_helper.rb
@@ -1,5 +1,8 @@
 module TodoItemsHelper
   def svg_object(name)
-    "<object data=\"#{image_path(name + '.svg')}\" type=\"image/svg+xml\">#{image_tag(name + '.png')}</object>".html_safe
+    # Why the extra span, bro?
+    # Because we want to be able to wrap it in a link.
+    # See: http://stackoverflow.com/a/12732030/209190
+    "<span><object data=\"#{image_path(name + '.svg')}\" type=\"image/svg+xml\">#{image_tag(name + '.png')}</object></span>".html_safe
   end
 end
