034fb3931920f33dd01774df4f712b92a42735a3
Erik Michaels-Ober <sferik@gmail.com>
2013-05-16 08:17:37 -0700

    Cleanup JSLint warnings

--- app/assets/javascripts/todo_items.js
+++ app/assets/javascripts/todo_items.js
@@ -1,5 +1,5 @@
-$(document).ready(function(){
-  function updateItem(container, complete, checkbox){
+$(document).ready(function () {
+  function updateItem(container, complete, checkbox) {
     var li = $(checkbox).parents('li');
     var id = li.attr('data-id');
     $.ajax({
@@ -7,20 +7,21 @@ $(document).ready(function(){
       method: 'put',
       data: {todo_item: {completed: complete}},
       dataType: 'json',
-      success: function(){
+      success: function () {
         var list = $(container);
         list.append(li);
       }
     });
   }
 
-  $('#todo').on('click', 'li input[type=checkbox]', function(){updateItem('#completed', true, this);});
-  $('#completed').on('click', 'li input[type=checkbox]', function(){updateItem('#todo', false, this);});
+  $('#todo').on('click', 'li input[type=checkbox]', function () { updateItem('#completed', true, this); });
+
+  $('#completed').on('click', 'li input[type=checkbox]', function () { updateItem('#todo', false, this); });
 
   var allowSubmit = true;
-  $('form').on('submit', function(event){
+  $('form').on('submit', function (event) {
     event.preventDefault();
-    if(!allowSubmit) { return false }
+    if (!allowSubmit) { return false; }
     allowSubmit = false;
     var form = $(this);
     var input = $('#todo_item_name');
@@ -36,29 +37,29 @@ $(document).ready(function(){
       method: form.attr('method'),
       data: {todo_item: item},
       dataType: 'json',
-      beforeSend: function(){
-        timer = setTimeout(function(){$('.spinner').fadeIn();}, 1000);
+      beforeSend: function () {
+        timer = setTimeout(function () { $('.spinner').fadeIn(); }, 1000);
       },
-      complete: function(){
+      complete: function () {
         allowSubmit = true;
         clearTimeout(timer);
         $('.spinner').fadeOut();
       },
-      success: function(todo){
+      success: function (todo) {
         var list = $('#todo');
         var entry = $('<li data-id="' + todo.id + '"></li>');
-        var checkbox = $('<span class="item_checkbox"><input type="checkbox"></span>');
-        var name = $('<span class="item_name"> ' + todo.name + ' </span>');
-        var time = $('<time datetime="' + todo.due_at + '" class="item_due_at"> ' + moment(todo.due_at).format('dddd, MMMM D, YYYY') + ' </time>');
-        var deleteButton = $('<span class="item_delete"><a href="/todo_items/' + todo.id + '" data-confirm="Are you sure?" data-method="delete" rel="nofollow"><span><object data="/assets/trash.svg" type="image/svg+xml"><img alt="Trash" src="/assets/trash.png" /></object></span></a></span>');
+        var checkbox = $('<span class="item_checkbox"><input type="checkbox"></span>').append(' ');
+        var name = $('<span class="item_name">' + todo.name + ' </span>').append(' ');
+        var time = $('<time datetime="' + todo.due_at + '" class="item_due_at">' + moment(todo.due_at).format('dddd, MMMM D, YYYY') + '</time>').append(' ');
+        var deleteButton = $('<span class="item_delete"><a href="/todo_items/' + todo.id + '" data-confirm="Are you sure?" data-method="delete" rel="nofollow"><span><object data="/assets/trash.svg" type="image/svg+xml"><img alt="Trash" src="/assets/trash.png"></object></span></a></span>');
         entry.append(checkbox, name, time, deleteButton);
         entry.appendTo(list);
         input.val('');
         input.focus();
       },
-      error: function(){
+      error: function () {
         $('.error').slideDown();
-        setTimeout(function(){$('.error').slideUp();}, 3000);
+        setTimeout(function () { $('.error').slideUp(); }, 3000);
       }
     });
   });
